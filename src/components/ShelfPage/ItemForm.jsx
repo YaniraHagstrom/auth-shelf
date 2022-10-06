f7d6f59055666b284702f2c2f7931617639091df
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-10-06 10:00:25 -0500

    add inputs and button

--- src/components/ShelfPage/ItemForm.jsx
+++ src/components/ShelfPage/ItemForm.jsx
@@ -1,2 +1,49 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-export default 
+function ItemForm() {
+
+    const dispatch = useDispatch();
+
+    const [newItem, setNewItem] = useState('');
+    const [newItemDescription, setNewItemDescription] = useState('');
+    const [newItemImage, setNewItemImage] = useState('');
+    
+
+    const handleAddItem = () => {
+        console.log('in handleAddItem');
+    }
+
+    return (
+        <div>
+            <h3>
+                Add an Item!
+            </h3>
+            <div>
+                <TextField 
+                value={newItemDescription}
+                onChange={(e) => setNewItemDescription(e.target.value)}
+                id="filled-basic" 
+                label="description" 
+                variant="filled" />
+            </div>
+            <div>
+                <TextField 
+                value={newItemImage}
+                onChange={(e) => setNewItemImage(e.target.value)}
+                id="filled-basic" 
+                label="image url" 
+                variant="filled" />
+            </div>
+            <div>
+                <Button onClick={handleAddItem} variant="contained">Add to Shelf</Button>
+            </div>
+        </div>
+    )
+
+}
+
+
+export default ItemForm;
